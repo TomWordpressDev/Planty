f6b1e2a1f21b8b7935349c9b422ad33ed49f0af4
Tom Soler <tom.a.soler@outlook.com>
2023-12-16 10:07:54 +0100

    modification du lien commander dans la barre de nav

--- wp-content/themes/Planty/functions.php
+++ wp-content/themes/Planty/functions.php
@@ -6,6 +6,7 @@ function theme_enqueue_styles()
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
 }
+
 add_filter('wp_nav_menu_items','add_admin_link', 10, 2);
 
 function add_admin_link( $items, $args ) 
